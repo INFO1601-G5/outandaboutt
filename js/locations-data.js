c03c18a90d57646ceba269d58aa04d4026d8771c
Aaron Payne <aaron.payne@my.uwi.edu>
2021-04-11 18:43:50 -0400

    Added JS for Locations

---
+++ js/locations-data.js
@@ -0,0 +1,95 @@
+/* This javascript file contains an array of all the location objects catalogued for this website.
+Each location object has follows the following properties:
+    id: '',
+    name:'',
+    img_url:'',  
+    coords: {
+        latitude: '',
+        longitude: '',
+    },
+    description:'',
+    tags: {
+        type: [''],
+        popularity: '',
+    },
+    likes: ''
+*/
+
+let locations = [
+    {id:"1",
+    name:"Fort King George",
+    img_url:"http://nationaltrust.tt/home/wp-content/uploads/2016/01/Tobago-Fort-King-George-7-1024x678.jpg?x65015",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["adventure","tobago"],
+        popularity: 1,
+    },
+    likes: ""
+},
+
+    {id:"2",
+    name:"Maracas Beach",
+    img_url:"https://media.tacdn.com/media/attractions-splice-spp-674x446/07/a9/ab/bf.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["beach","relax", "fun"],
+        popularity: 1,
+    },
+    likes: ""
+},
+
+    {id:"3",
+    name:"Bamboo Cathedral",
+    img_url:"https://c2.staticflickr.com/6/5080/5908334143_43d6a4f789_b.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["beach","fun"],
+        popularity: 5,
+    },
+    likes: ""
+},
+
+    {id:"4",
+    name:"Movietowne Mall POS",
+    img_url: "https://res.cloudinary.com/terracaribbean/image/upload/t_webi_display/terra-red/trinidad/photos/t28035/68f5cbaa6b32ed49b5857a312c51a806.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["restaurant","relax", "fun"],
+        popularity: 1,
+    },
+    likes: ""
+},
+
+    {id:"5",
+    name:"Lopinot Historical Complex",
+    img_url:"http://nationaltrust.tt/home/wp-content/uploads/2016/01/Lopinot-5-1024x768.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["culture","building"],
+        popularity: 1,
+    },
+    likes: ""
+
+},
+
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+      let map;
+      let place = locations[0];
+    
+
+
+      function featuredPlaces(){
+        let placeid = Math.floor(Math.random() * 11);
+        let html='';
+        let featuredPlace=locations[placeid-1];
+        html += `<div class="col">
+          <img src= "${featuredPlace.img_url}" width = "50%" height = "50%">
+                </div>
+                <div class="col">
+                  <p> Name: ${featuredPlace.name}</p>
+                  <p> Description: ${featuredPlace.description}</p>
+                </div>`;
+        document.querySelector('#myCarousel').innerHTML = html;
+        
+
+
+        showFeaturedLocation(locations[placeid-1])
+      }
+ 
+
+      function initMap(x,y){
+        map = new google.maps.Map(document.getElementById("map"), {
+          //center: {lat: locations[0].latitude, lng: locations[0].longitude},
+          center: {lat: x,lng: y},
+          zoom: 8,
+        });
+      }
+
+      function showLocation(location){
+        let html = '';
+
+        html += `<div class="col">
+          <img src= "${location.img_url}" width = "50%" height = "50%">
+                </div>
+                <div class="col">
+                  <p> Name: ${location.name}</p>
+                  <p> Description: ${location.description}</p>
+                </div>`;
+        document.querySelector('#placeDetail').innerHTML = html;
+        
+        const x = new Number(location.latitude);
+        const y = new Number (location.longitude);
+        initMap(x,y);
+      }
+      //initMap(50,80)
+      showLocation(place);
+
+      function getLocation(id){
+        showLocation(place);
+      }
 
