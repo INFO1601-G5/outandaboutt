fa50d2266d6696e040cf807d817c031d46c2ac57
Aaron Payne <aaron.payne@my.uwi.edu>
2021-04-12 10:20:29 -0400

    Updated locations

--- js/locations-data.js
+++ js/locations-data.js
@@ -25,7 +25,7 @@ let locations = [
     },
     description:"insert description here",
     tags: {
-        type: ["adventure","tobago"],
+        type: ["adventure","tobago", "historical"],
         popularity: 1,
     },
     likes: ""
@@ -41,7 +41,7 @@ let locations = [
     description:"insert description here",
     tags: {
         type: ["beach","relax", "fun"],
-        popularity: 1,
+        popularity: 5,
     },
     likes: ""
 },
@@ -55,7 +55,7 @@ let locations = [
     },
     description:"insert description here",
     tags: {
-        type: ["beach","fun"],
+        type: ["adventure","fun"],
         popularity: 5,
     },
     likes: ""
@@ -70,8 +70,8 @@ let locations = [
     },
     description:"insert description here",
     tags: {
-        type: ["restaurant","relax", "fun"],
-        popularity: 1,
+        type: ["restaurant","dine","urban"],
+        popularity: 5,
     },
     likes: ""
 },
@@ -85,8 +85,343 @@ let locations = [
     },
     description:"insert description here",
     tags: {
-        type: ["culture","building"],
+        type: ["historical","adventure"],
         popularity: 1,
+    },
+    likes: ""
+
+},
+    {id:"6",
+    name:"Paria Waterfall",
+    img_url:"http://nationaltrust.tt/home/wp-content/uploads/2016/01/Lopinot-5-1024x768.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"",
+    tags: {
+        type: ["wetlands","adventure","beach"],
+        popularity: 1,
+    },
+    likes: ""
+
+},
+
+    {id:"7",
+    name:"Mount St. Benedict",
+    img_url:"http://nationaltrust.tt/home/wp-content/uploads/2016/01/Lopinot-5-1024x768.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["historical","adventure"],
+        popularity: 3,
+    },
+    likes: ""
+
+},
+
+    {id:"6",
+    name:"Avocat Waterfall",
+    img_url:"https://www.destinationtnt.com/wp-content/uploads/avocat-waterfall_P1040593.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["wetlands","adventure","fun"],
+        popularity: 3,
+    },
+    likes: ""
+
+},
+
+    {id:"8",
+    name:"Turtle Watching (Matura Bay)",
+    img_url:"https://www.destinationtnt.com/wp-content/uploads/fs_turtle-nesting-season-059u1741-1600x1064.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","adventure","beach"],
+        popularity: 3,
+    },
+    likes: ""
+
+},
+
+    {id:"9",
+    name:"Nylon Pool",
+    img_url:"https://rustytraveltrunk.com/wp-content/uploads/2016/11/Tobago-Nylon-Pool-5.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["adventure","relax","tobago"],
+        popularity: 4,
+    },
+    likes: ""
+
+},
+
+    {id:"10",
+    name:"Ohana Villa",
+    img_url:"http://realestatett.com/wp-content/uploads/2016/12/unspecified-2-2016-12-02-at-12-07-26.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["tobago","relax","urban"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"11",
+    name:"Oceans Salybia Beach Resort",
+    img_url:"https://trinidadandtobagovillas.net/account/propertyImages/119/2017-07-27%2018_51_24%20-%207INu2X1.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["relax","urban","beach"],
+        popularity: 3,
+    },
+    likes: ""
+
+},
+
+    {id:"12",
+    name:"TGI Fridays",
+    img_url:"https://media-cdn.tripadvisor.com/media/photo-s/09/0d/2f/64/tgi-fridays.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","urban","dine"],
+        popularity: 5,
+    },
+    likes: ""
+
+},
+
+    {id:"13",
+    name:"Town Resturant and Bar",
+    img_url:"https://caribbean-restaurants.top/storage/app/uploads/public/5f8/f3a/062/5f8f3a06244ad862050378.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["urban","dine"],
+        popularity: 4,
+    },
+    likes: ""
+
+},
+
+    {id:"14",
+    name:"Samurai Restaurant",
+    img_url:"http://www.samuraitt.com/wp-content/uploads/2016/12/experience2.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["urban","dine"],
+        popularity: 1,
+    },
+    likes: ""
+
+},
+
+    {id:"15",
+    name:"Covigne River Gorge",
+    img_url:"https://pbs.twimg.com/media/D1JFkg4X0AAsR5K?format=jpg&name=large",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","adventure","wetlands"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"16",
+    name:"Argyle WaterFall",
+    img_url:"https://img.17qq.com/images/arehusuawax.jpeg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["wetlands","adventure","tobago"],
+        popularity: 1,
+    },
+    likes: ""
+
+},
+
+    {id:"17",
+    name:"Black Rock Beach",
+    img_url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/7b/84/8d/view.jpg?w=1000&h=600&s=1",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["tobago","adventure","beach"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"18",
+    name:"Papadums Indian Joint (Traditional Indian Cuisine)",
+    img_url:"https://images.squarespace-cdn.com/content/v1/57fe9d39e4fcb57a5fb0b428/1483220704301-HJW7WF39JXB2MI0AFV24/ke17ZwdGBToddI8pDm48kDHPSfPanjkWqhH6pl6g5ph7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5dNb1QJw9casjKdtTg1_-y4jz4ptJBmI9gQmbjSQnNGng/Trailer.JPG?format=1500w",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","urban","dine"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"19",
+    name:"Maracas Waterfall",
+    img_url:"https://64.media.tumblr.com/03f70d4ae66f0517c4122490de4c9ace/tumblr_nzb2dnqrjl1v0ah94o1_1280.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["wetlands","adventure","beach"],
+        popularity: 3,
+    },
+    likes: ""
+
+},
+
+    {id:"20",
+    name:"Yarra Beach",
+    img_url:"https://www.destinationtnt.com/wp-content/uploads/yarra-beach-IMG_3283-1.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","relax","beach"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"21",
+    name:"La Foret Beach",
+    img_url:"https://i.imgur.com/QHZ5cLo.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","adventure","beach"],
+        popularity: 1,
+    },
+    likes: ""
+
+},
+
+    {id:"22",
+    name:"Caura River",
+    img_url:"https://loopnewslive.blob.core.windows.net/liveimage/sites/default/files/2020-04/WiJUVo4MJ4.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","relax","wetlands"],
+        popularity: 5,
+    },
+    likes: ""
+
+},
+
+    {id:"23",
+    name:"Columbus Bay",
+    img_url:"https://www.destinationtnt.com/wp-content/uploads/Columbus-Bay_IMG72-5-1600x1067.jpg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","relax","beach"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"24",
+    name:"Grande Riviere",
+    img_url:"https://deih43ym53wif.cloudfront.net/boat-grande-riviere-trinidad-shutterstock_12520948_5b4030346e.jpeg",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["fun","adventure","beach"],
+        popularity: 2,
+    },
+    likes: ""
+
+},
+
+    {id:"25",
+    name:"Pitch Lake, La Brea",
+    img_url:"https://lh3.ggpht.com/-ukEFExIxV10/UHmAiTuAsbI/AAAAAAAAe7M/hTwnKkoayug/w1200-h630-p-k-no-nu/pitch-lake-1%25255B6%25255D.jpg?imgmax=800",
+    coords: {
+        latitude: 10.046682,
+        longitude: -61.920396,
+    },
+    description:"insert description here",
+    tags: {
+        type: ["historical","adventure"],
+        popularity: 3,
     },
     likes: ""
 
