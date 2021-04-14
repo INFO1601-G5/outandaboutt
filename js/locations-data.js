0cb5647ead9ab5c8dad4e20284a443cb8070f675
Aaron Payne <aaron.payne@my.uwi.edu>
2021-04-14 08:56:54 -0400

    Locations fix 14-04-21

--- js/locations-data.js
+++ js/locations-data.js
@@ -300,7 +300,7 @@ let locations = [
 },
 
     {id:"18",
-    name:"Papadums Indian Joint (Traditional Indian Cuisine)",
+    name:"Papadums Indian Food Joint",
     img_url:"https://images.squarespace-cdn.com/content/v1/57fe9d39e4fcb57a5fb0b428/1483220704301-HJW7WF39JXB2MI0AFV24/ke17ZwdGBToddI8pDm48kDHPSfPanjkWqhH6pl6g5ph7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5dNb1QJw9casjKdtTg1_-y4jz4ptJBmI9gQmbjSQnNGng/Trailer.JPG?format=1500w",
     coords: {
         latitude: 10.046682,
