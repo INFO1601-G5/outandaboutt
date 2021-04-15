a2562baf0b7c422b3433f48da8db7bedf7121973
Kristian480 <>
2021-04-15 01:11:54 +0000

    Search bar

--- js/site_Buttons.js
+++ js/site_Buttons.js
@@ -1,6 +1,6 @@
 //url=explore.html
 async function loadList(locations,tag){
-    let locList=document.querySelector("#Tester");
+    let locList=document.querySelector("#homeinfo");
     let html="";
 
     let tagArr=locations.filter(location => {
@@ -15,4 +15,38 @@ async function loadList(locations,tag){
    locList.innerHTML=html;
   }
 
+ async function loadExplore(tagList){
+    let explore=document.querySelector("#homeinfo");
+    let html="";
+
+    for(let tagg of tagList){
+      html+=`<ul>
+      <li><a href="#" onclick="loadList(locations,'${tagg}')">${tagg}</a></li>
+      </ul>`;
+    }
+    explore.innerHTML=html;
+  }
  
+ function getName(){
+    let place_name=document.querySelector(".search-bar");
+    let placeName;
+      return place_name.value;
+  }
+  async function searchbut(locations){
+    //console.log("HERE");
+    const name=getName().toUpperCase();
+    let locSearch=document.querySelector("#homeinfo");
+    let html="";
+
+    let nameArr=locations.filter(location => {
+  return location.name.toUpperCase().includes(`${name}`);
+});
+
+ for(let names of nameArr){
+     console.log(names.name);
+     html+=`<ul>
+      <li><a href="#" onclick="">${names.name}</a></li>
+      </ul`;
+   }
+   locSearch.innerHTML=html;
+  }
